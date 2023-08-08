87bd9f3819f275353c7fa5b7daca5af1e0ce2c1a
Tao Chen <taochensyd@gmail.com>
2023-08-08 14:23:22 +1000

    add dotenv config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+require('dotenv').config()
 app.use(express.json());
 const port = 3005;
+
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -18,6 +20,44 @@ app.use((req, res, next) => {
 const sessionObj = {
   sessionId: null,
   sessionTime: null,
+};
+
+const loginToSAPSession = async () => {
+  try {
+    const response = await axios.post(
+      `${process.env.SAP_BASE_URL}/b1s/v1/Login`,
+      {
+        CompanyDB: process.env.DB_NAME,
+        UserName: process.env.SAP_USERNAME,
+        Password: process.env.SAP_PASSWORD,
+      }
+    );
+    console.log(
+      "response.headers['set-cookie']:",
+      response.headers["set-cookie"]
+    );
+    console.log("response.data:" + response.data);
+
+    const cookies = response.headers["set-cookie"];
+    const b1SessionCookie = cookies
+      .find((cookie) => cookie.startsWith("B1SESSION="))
+      .split(";")[0];
+    const routeIdCookie = cookies
+      .find((cookie) => cookie.startsWith("ROUTEID="))
+      .split(";")[0];
+    const cookie = [b1SessionCookie, routeIdCookie].join("; ");
+
+    sessionObj.sessionId = cookie;
+    if (response.status === 200) {
+      console.log("Logged in to SAP session");
+      return response.status;
+    } else {
+      console.log("Failed to login to SAP session");
+      return response.status;
+    }
+  } catch (error) {
+    console.log("error.message:" + error.message);
+  }
 };
 
 
