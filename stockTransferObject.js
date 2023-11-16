7f4877ca586d366a7d587b9bf7fc26b3b717f833
Tao Chen <taochensyd@gmail.com>
2023-11-16 12:31:31 +1100

    add stock transfer object placeholder

---
+++ stockTransferObject.js
@@ -0,0 +1,33 @@
+// {
+//     JournalMemo: WEB STOCK Transfer 202311160003,
+//     Comments: remark,
+//     StockTransferLines: [
+//         {
+//             ItemCode: PKKEY-0005,
+//             Quantity: quantity,
+//             FromWarehouseCode: WCP,
+//             WarehouseCode: WCP,
+//             SerialNumbers: [],
+//             BatchNumbers: [
+//                 {
+//                     BatchNumber: searchTerm,
+//                     Quantity: quantity
+//                 }
+//             ],
+//             StockTransferLinesBinAllocations: [
+//                 {
+//                     BinAbsEntry: 805,
+//                     BinActionType: "batFromWarehouse",
+//                     Quantity: quantity,
+//                     SerialAndBatchNumbersBaseLine: 0
+//                 },
+//                 {
+//                     BinAbsEntry: 806,
+//                     BinActionType: "batToWarehouse",
+//                     Quantity: quantity,
+//                     SerialAndBatchNumbersBaseLine: 0
+//                 }
+//             ]
+//         }
+//     ]
+// }
