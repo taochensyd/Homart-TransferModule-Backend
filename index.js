65ebb7a6ff8c0919b1f455cd0850bd9a3fb1670a
Tao Chen <taochensyd@gmail.com>
2023-11-22 16:45:07 +1100

    fetch bin location every 10 minutes

--- index.js
+++ index.js
@@ -22,33 +22,30 @@ const port = 3005;
  */
 const sessionObj = { sessionId: null, sessionTime: null },
   loginToSAPSession = async () => {
-    if ("HOME" !== process.env.ENVIRONMENT)
-      try {
-        const s = await axios.post(process.env.SAP_BASE_URL + "/b1s/v1/Login", {
-          CompanyDB: process.env.DB_NAME,
-          UserName: process.env.SAP_USERNAME,
-          Password: process.env.SAP_PASSWORD,
-        });
-        console.log("response.headers['set-cookie']:", s.headers["set-cookie"]),
-          console.log("response.data:" + s.data);
-        const e = s.headers["set-cookie"],
-          o = [
-            e.find((s) => s.startsWith("B1SESSION=")).split(";")[0],
-            e.find((s) => s.startsWith("ROUTEID=")).split(";")[0],
-          ].join("; ");
-        return (
-          (sessionObj.sessionId = o),
-          200 === s.status
-            ? (console.log("Logged in to SAP session"), s.status)
-            : (console.log("Failed to login to SAP session"), s.status)
-        );
-      } catch (s) {
-        console.log("error.message:" + s.message);
-      }
+    try {
+      const s = await axios.post(process.env.SAP_BASE_URL + "/b1s/v1/Login", {
+        CompanyDB: process.env.DB_NAME,
+        UserName: process.env.SAP_USERNAME,
+        Password: process.env.SAP_PASSWORD,
+      });
+      console.log("response.headers['set-cookie']:", s.headers["set-cookie"]),
+        console.log("response.data:" + s.data);
+      const e = s.headers["set-cookie"],
+        o = [
+          e.find((s) => s.startsWith("B1SESSION=")).split(";")[0],
+          e.find((s) => s.startsWith("ROUTEID=")).split(";")[0],
+        ].join("; ");
+      return (
+        (sessionObj.sessionId = o),
+        200 === s.status
+          ? (console.log("Logged in to SAP session"), s.status)
+          : (console.log("Failed to login to SAP session"), s.status)
+      );
+    } catch (s) {
+      console.log("error.message:" + s.message);
+    }
   };
 app.post("/api/login", async (s, e) => {
-  "HOME" === process.env.ENVIRONMENT &&
-    e.status(200).send({ message: "Logged in to SAP session" });
   let o = !1;
   (o = await loginToSAPSession()),
     200 === o
@@ -56,8 +53,6 @@ app.post("/api/login", async (s, e) => {
       : e.status(o).send({ message: "Failed to login to SAP session" });
 }),
   app.post("/api/userAccount", async (s, e) => {
-    if ("HOME" === process.env.ENVIRONMENT)
-      return e.status(200).json({ count: 1 });
     console.log("req.body:", s.body);
     const o = `https://192.168.0.44:50000/b1s/v1/view.svc/Homart_CheckUserAccount_B1SLQuery()?$filter=U_UserCode eq '${s.body.sapusername}' and U_UserPW eq '${s.body.sappassword}'`;
     console.log(o);
@@ -75,58 +70,54 @@ app.post("/api/login", async (s, e) => {
     } catch (s) {
       console.log("error.message:", s.message), e.status(500).send(s.message);
     }
-  }),
-  app.post("/api/batchnumberdetail", async (s, e) => {
-    if ("HOME" === process.env.ENVIRONMENT)
-      return e.status(200).json({
-        "odata.metadata":
-          "https://192.168.0.44:50000/b1s/v1/$metadata#BatchNumberDetails",
-        value: [
-          {
-            DocEntry: 19425,
-            ItemCode: "PB300-0010",
-            ItemDescription: "Bottle 1HOM RA300ml Amber PET Bottle - JX",
-            Status: "bdsStatus_Released",
-            Batch: "H36592",
-            BatchAttribute1: "S00403-Homart OEM",
-            AdmissionDate: "2023-03-08",
-            SystemNumber: 89,
-            U_DateReleased: "2023-03-31",
-            U_ClaimedQty: 19040,
-            U_WarehouseComment: "(2P*16C+12C+8C)*272+(28C+8C)*136=19040",
-            U_SupplierCode: "S00014-浙江上虞市佳星塑料製品有限公司 RMB/JX",
-            U_InventoryUoM: "each",
-          },
-        ],
-      });
-    console.log("req.body:", s.body);
-    const o = `https://192.168.0.44:50000/b1s/v1/BatchNumberDetails?$filter=Batch eq '${s.body.BatchNumber}'`;
-    console.log(o),
-      console.log("sessionObj.sessionId: " + sessionObj.sessionId);
+  });
+  // app.post("/api/batchnumberdetail", async (s, e) => {
+  //   console.log("req.body:", s.body);
+  //   const o = `https://192.168.0.44:50000/b1s/v1/BatchNumberDetails?$filter=Batch eq '${s.body.BatchNumber}'`;
+  //   console.log(o),
+  //     console.log("sessionObj.sessionId: " + sessionObj.sessionId);
+  //   try {
+  //     const s = await axios.get(o, {
+  //       withCredentials: !0,
+  //       headers: {
+  //         Cookie: sessionObj.sessionId,
+  //         Prefer: "odata.maxpagesize=9999999999",
+  //       },
+  //     });
+  //     console.log("/b1s/v1/BatchNumberDetails"), e.send(s.data);
+  //   } catch (s) {
+  //     console.log("error.message:", s.message), e.status(500).send(s.message);
+  //   }
+  // }),
+
+
+  let batchNumberDetail = null;
+
+  setInterval(async () => {
     try {
-      const s = await axios.get(o, {
-        withCredentials: !0,
+      const response = await axios.get(`https://192.168.0.44:50000/b1s/v1/BatchNumberDetails?$filter=Batch eq '${s.body.BatchNumber}'`, {
+        withCredentials: true,
         headers: {
           Cookie: sessionObj.sessionId,
           Prefer: "odata.maxpagesize=9999999999",
         },
       });
-      console.log("/b1s/v1/BatchNumberDetails"), e.send(s.data);
-    } catch (s) {
-      console.log("error.message:", s.message), e.status(500).send(s.message);
+      batchNumberDetail = response.data;
+    } catch (error) {
+      console.log("Error fetching batch number detail:", error.message);
     }
-  }),
+  }, 600000); // 600000 milliseconds = 10 minutes
+  
+  app.post("/api/batchnumberdetail", (req, res) => {
+    if (batchNumberDetail) {
+      res.send(batchNumberDetail);
+    } else {
+      res.status(500).send("No data available");
+    }
+  });
+  
+
   app.post("/api/items", async (s, e) => {
-    if ("HOME" === process.env.ENVIRONMENT)
-      return e.status(200).json({
-        "odata.metadata":
-          "https://192.168.0.44:50000/b1s/v1/$metadata#Items/@Element",
-        "odata.etag": 'W/"DA4B9237BACCCDF19C0760CAB7AEC4A8359010B0"',
-        ItemCode: "PLPID-0006",
-        ItemName: "Label 1BSP Performance Inspired Turmeric Curcumin 120s",
-        ForeignName: "Label Performance Inspired Turmeric Curcumin 120s",
-        InventoryUOM: "each",
-      });
     console.log("req.body:", s.body);
     const o = `https://192.168.0.44:50000/b1s/v1/Items('${s.body.ItemNumber}')`;
     console.log(o),
@@ -193,31 +184,6 @@ app.post("/api/batchinbin", async (s, e) => {
     console.log("error.message:", s.message), e.status(500).send(s.message);
   }
 }),
-  app.post("/api/journalmemo", async (s, e) => {
-    if ("HOME" === process.env.ENVIRONMENT)
-      return e.status(200).json({
-        "odata.metadata":
-          "https://192.168.0.44:50000/b1s/v1/view.svc/$metadata#Homart_JournalMemo_B1SLQuery",
-        value: [{ JrnlMemo: "WEB STOCK Transferno:202307280005", id__: 1 }],
-      });
-    console.log("req.body:", s.body);
-    const o =
-      "https://192.168.0.44:50000/b1s/v1/view.svc/Homart_JournalMemo_B1SLQuery()";
-    console.log(o), console.log("getJournalMemoBaseURL: " + o);
-    try {
-      const s = await axios.get(o, {
-        withCredentials: !0,
-        headers: {
-          Cookie: sessionObj.sessionId,
-          Prefer: "odata.maxpagesize=9999999999",
-        },
-      });
-      console.log("/b1s/v1/view.svc/Homart_JournalMemo_B1SLQuery()"),
-        e.send(s.data);
-    } catch (s) {
-      console.log("error.message:", s.message), e.status(500).send(s.message);
-    }
-  }),
   app.post("/api/nextavailablejournalmemo", async (s, e) => {
     const today = new Date();
     const year = today.getFullYear();
